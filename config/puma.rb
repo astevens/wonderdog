051ba4a6b3b7c808686bc65efed8097c62c96ef1
Andy Stevens <andy@orangesix.com>
2014-10-16 17:45:29 -0500

    add puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,32 +1,8 @@
-#!/usr/bin/env puma
-pidfile 'tmp/pids/puma.pid'
-state_path 'tmp/pids/puma.state'
+workers Integer(ENV['PUMA_WORKERS'] || 3)
+threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 16)
 
-threads 0, 16
+preload_app!
 
-bind 'tcp://0.0.0.0:9292'
-bind 'unix://tmp/sockets/puma.sock?umask=0777'
-
-# Command to use to restart puma. This should be just how to
-# load puma itself (ie. 'ruby -Ilib bin/puma'), not the arguments
-# to puma, as those are the same as the original process.
-#
-# restart_command '/u/app/lolcat/bin/restart_puma'
-
-# workers 2
-
-# on_worker_boot do
-#   puts 'On worker boot...'
-# end
-
-# after_worker_boot do
-#   puts 'On worker boot...'
-# end
-
-# prune_bundler
-# preload_app!
-# worker_timeout 60
-
-# tag 'app name'
-
-# activate_control_app 'unix://tmp/sockets/pumactl.sock'
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
